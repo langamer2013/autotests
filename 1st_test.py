6696077a65828bd4ccc0ba6e70b98f97c99f04bd
langamer2013 <langamer2013@gmail.com>
2025-12-03 00:27:55 +0700

    need to learn

--- 1st_test.py
+++ 1st_test.py
@@ -1,3 +1,22 @@
 
-import pprint
-print("hello")
+from pprint import pprint
+import paramiko
+import time
+from concurrent.futures import ThreadPoolExecutor
+
+client_ip = "192.168.50.252"
+server_ip = ""
+username = "tester"
+password = "tester"
+
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+client.connect(client_ip, username="tester", password="tester", look_for_keys=False, allow_agent=False)
+
+ssh = client.invoke_shell()
+ssh.send("ping -c5 192.168.50.251\n")
+time.sleep(6)
+
+a = ssh.recv(3000).decode("utf-8")
+print(type(a))
+pprint(a.split("\n"))
