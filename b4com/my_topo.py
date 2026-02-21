4e6c32ed3014f43f90f63f27c467846a63737ebd
langamer2013 <langamer2013@gmail.com>
2026-02-21 14:06:27 +0700

    b4com 5st

--- b4com/my_topo.py
+++ b4com/my_topo.py
@@ -3,7 +3,7 @@ from mininet.node import Host
 from mininet.cli import CLI
 from mininet.log import setLogLevel, info
 
-PACKET_TO_SEND = 1000
+NUMBER_OF_PACKETS_TO_SEND = 1000
 
 def get_packet_count(device, interface):
     tx_packets = device.cmd(f'cat /sys/class/net/{interface}/statistics/tx_packets')
@@ -52,8 +52,7 @@ def mynetwork():
     r2.cmd('ip route add default via 10.0.3.2')
     r3.cmd('ip ro add default via 10.0.4.2')
     # h2.cmd('ip route add default via 10.0.0.4') # По умолчанию через r2
-    h1.cmd(
-        'python3 -c "from scapy.all import *; sendp(Ether()/IP(dst=\'1.1.1.1\', src=RandIP(\'10.0.0.128/25\'))/TCP(dport=80, sport=444, flags=\'S\'), iface=\'h1-eth0\', count=PACKET_TO_SEND)"')
+    h1.cmd(f'\'python3 -c "from scapy.all import *; sendp(Ether()/IP(dst=\'1.1.1.1\', src=RandIP(\'10.0.0.128/25\'))/TCP(dport=80, sport=444, flags=\'S\'), iface=\'h1-eth0\', count={NUMBER_OF_PACKETS_TO_SEND})"\'')
     tx_interface1 = get_packet_count(r1, 'r1-eth1')
     tx_interface2 = get_packet_count(r1, 'r1-eth2')
     print(tx_interface1)
