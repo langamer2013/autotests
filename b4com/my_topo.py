fa9da8031dd914d2bfd82b1bab5585d202b8e122
langamer2013 <langamer2013@gmail.com>
2026-02-21 13:29:41 +0700

    b4com 5st

--- b4com/my_topo.py
+++ b4com/my_topo.py
@@ -49,7 +49,7 @@ def mynetwork():
 
     CLI(net)
     h1.cmd(
-        'python3 -c "from scapy.all import *; send(IP(dst=\'1.1.1.1\', src=RandIP(\'10.0.0.128/25\', sport=444 ))/TCP(dport=80, flags=\'S\'), count=5000)"')
+        'python3 -c "from scapy.all import *; send(IP(dst=\'1.1.1.1\', src=RandIP(\'10.0.0.128/25\', sport=444 ))/TCP(dport=80, flags=\'S\'), count=100)"')
     net.stop()
 
 
